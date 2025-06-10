9b318dc4ff81ad66c57398a15b5fc81a430c5877
HudsonCarneiro <161895030+HudsonCarneiro@users.noreply.github.com>
2025-06-10 12:24:46 -0300

    PostingAPP add main.js && router/index.js

--- PostingAPP/src/router/index.js
+++ PostingAPP/src/router/index.js
@@ -1,23 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import ForgotPassword from '../views/ForgotPasswordView.vue'
 import HomeView from '../views/HomeView.vue'
 
+const routes = [
+  { path: '/login', component: LoginView },
+  { path: '/register', component: RegisterView },
+  { path: '/', component: PostsView }, // pagina principal apos login
+
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory({
+    history: createWebHistory(),
+    routes,
+  })
 })
 
 export default router
